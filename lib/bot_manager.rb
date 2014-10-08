164fd210576cbe409aaeed03b07f011d3e5f5f69
Daniel Bucher <daniel.bucher88@gmail.com>
2014-10-08 20:08:58 -0300

    Add bot_manager class
    Bot manager creates and starts bots to listen on channels.

--- lib/bot_manager.rb
+++ lib/bot_manager.rb
@@ -3,10 +3,15 @@ require 'yaml'
 
 class BotManager
 
-  # Collection of bots
+  # A Array with all bots in it.
+  #
+  # return[Array]
   attr_reader :bots
 
-  # Load bots from config file
+  # Load bots from config file and add a configuration to each according to
+  # the config file.
+  #
+  # @return [void]
   def load_bots
     @bots = []
 
@@ -32,17 +37,52 @@ class BotManager
     end
   end
 
+  # Starts all bots.
+  #
+  # @return [void]
   def start_bots
     @bots.each {|bot| bot.start}
   end
 
   private
 
+  # Loads configuration file for bots
+  #
+  # @return [Hash] A hash containing all information in config/bots.yml
   def load_config
     YAML.load_file('../config/bots.yml')
   end
+
+  # Loads keywords from the appropriate config file
+  #
+  # @return [Array] The array of keywords
+  def keywords
+    unless @keywords
+      @keywords = YAML.load_file('../config/keywords.yml')['keywords']
+    end
+
+    @keywords
+  end
+
+  # Returns true if a message contains one of the keywords
+  #
+  # @param [String] message a message from a IRC channel
+  # @return [Boolean]
+  def has_keyword?(message)
+    @keywords.each do |w|
+      return true if message.include? w
+    end
+
+    false
+  end
+
+  # TODO: finish this
+  def save_message
+    nil
+  end
 end
 
+# The program will start here
 manager = BotManager.new
 manager.load_bots
 manager.start_bots
