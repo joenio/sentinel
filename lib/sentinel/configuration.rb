29a2a8a5a710fedccb92d1835122dd945a6a369d
Daniel Bucher <daniel.bucher88@gmail.com>
2014-10-14 16:56:42 -0300

    Add Sentinel module to Configuration

--- lib/sentinel/configuration.rb
+++ lib/sentinel/configuration.rb
@@ -1,11 +1,13 @@
 require 'yaml'
 
-module Configuration
+module Sentinel
+  module Configuration
 
-  # Loads configuration file for bots
-  #
-  # @return [Hash] A hash containing all information in config/bots.yml
-  def self.load_config_file
-    YAML.load_file('config/bots.yml')
+    # Loads configuration file for bots
+    #
+    # @return [Hash] A hash containing all information in config/bots.yml
+    def self.load_config_file
+      YAML.load_file('config/bots.yml')
+    end
   end
 end
